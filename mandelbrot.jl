0daea8d078fec85bff98dc6f64d7610d9310d984
SchwammPizza <ambros.anrig@gmail.com>
2021-06-16 09:02:04 +0300

    farbkreis

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -11,11 +11,56 @@ function iterationBerechung(c, list)
     end
 end
 
-for i = 1:n
-    for j = 1:m
-        y = (2*i - n)/n * 1im
-        x = (3*j - 2*m)/m
-        println(x + y)
+using Colors
 
+function farbkreis(i, max_iteration=100)
+    r = max_iteration/6
+
+    if i < 0
+        i += max_iteration
+    elseif i > max_iteration
+        i -= max_iteration
+    end
+    
+    if i < r
+        return RGB{Float64}(1, 0, i/r)
+    elseif  i == r
+        return RGB{Float64}(1, 0, 1)
+    elseif i < 2*r
+        return RGB{Float64}(1 - (i - r)/r, 0, 1)
+    elseif  i == 2*r
+        return RGB{Float64}(0, 0, 1)
+    elseif i < 3*r
+        return RGB{Float64}(0, (i - 2*r)/r, 1)
+    elseif i == 3*r
+        return RGB{Float64}(0, 1, 1)
+    elseif i < 4*r
+        return RGB{Float64}(0, 1, 1 - (i - 3*r)/r)
+    elseif i == 4*r
+        return RGB{Float64}(0, 1, 0)
+    elseif i < 5*r
+        return RGB{Float64}((i - 4*r)/r, 1, 0)
+    elseif i == 5*r
+        returnRGB{Float64}(1, 1, 0)
+    elseif i < max_iteration
+        return RGB{Float64}(1, (i - 5*r)/r, 0)
+    else
+        return RGB{Float64}(1, 0, 0)
     end
 end
+
+iteration = 100
+
+for i = 1:iteration
+    println(farbkreis(i, iteration), i)
+end
+
+
+# for i = 1:n
+#     for j = 1:m
+#         y = (2*i - n)/n * 1im
+#         x = (3*j - 2*m)/m
+#         println(x + y)
+
+#     end
+# end
