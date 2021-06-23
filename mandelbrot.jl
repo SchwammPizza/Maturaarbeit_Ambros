2475f84a4e34e764fdc3af9997c11f754e68aa01
SchwammPIzza <ambros.anrig@gmail.com>
2021-06-23 06:47:18 +0200

    ad time mesure

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -1,57 +1,50 @@
 using Images, Colors
 
-n = 720*113.5
-m = Int(n/720*1080)
-
-img = zeros(RGB{Float64},n,m)
-
-function iterationBerechung(c, list)
-    for i = 1:100
-        i
-    end
-end
-
-using Colors
-
-function farbkreis(i, max_iteration=100)
-    r = max_iteration/6
-
-    if i < 0
-        i += max_iteration
-    elseif i > max_iteration
-        i -= max_iteration
-    end
-    
-    if i < r
-        return RGB{Float64}(1, 0, i/r)
-    elseif  i == r
-        return RGB{Float64}(1, 0, 1)
-    elseif i < 2*r
-        return RGB{Float64}(1 - (i - r)/r, 0, 1)
-    elseif  i == 2*r
-        return RGB{Float64}(0, 0, 1)
-    elseif i < 3*r
-        return RGB{Float64}(0, (i - 2*r)/r, 1)
-    elseif i == 3*r
-        return RGB{Float64}(0, 1, 1)
-    elseif i < 4*r
-        return RGB{Float64}(0, 1, 1 - (i - 3*r)/r)
-    elseif i == 4*r
-        return RGB{Float64}(0, 1, 0)
-    elseif i < 5*r
-        return RGB{Float64}((i - 4*r)/r, 1, 0)
-    elseif i == 5*r
-        returnRGB{Float64}(1, 1, 0)
-    elseif i < max_iteration
-        return RGB{Float64}(1, (i - 5*r)/r, 0)
-    else
-        return RGB{Float64}(1, 0, 0)
-    end
-end
-
-iteration = 100
-
 @time begin
+    n = Int(720*35.9)
+    m = Int(n/720*1080)
+
+    img = zeros(RGB{Float64}, n, m)
+
+    function farbkreis(i, max_iteration=100)
+        r = max_iteration/6
+
+        if i < 0
+            i += max_iteration
+        elseif i > max_iteration
+            i -= max_iteration
+        end
+        
+        if i < r
+            return RGB{Float64}(1, 0, i/r)
+        elseif  i == r
+            return RGB{Float64}(1, 0, 1)
+        elseif i < 2*r
+            return RGB{Float64}(1 - (i - r)/r, 0, 1)
+        elseif  i == 2*r
+            return RGB{Float64}(0, 0, 1)
+        elseif i < 3*r
+            return RGB{Float64}(0, (i - 2*r)/r, 1)
+        elseif i == 3*r
+            return RGB{Float64}(0, 1, 1)
+        elseif i < 4*r
+            return RGB{Float64}(0, 1, 1 - (i - 3*r)/r)
+        elseif i == 4*r
+            return RGB{Float64}(0, 1, 0)
+        elseif i < 5*r
+            return RGB{Float64}((i - 4*r)/r, 1, 0)
+        elseif i == 5*r
+            returnRGB{Float64}(1, 1, 0)
+        elseif i < max_iteration
+            return RGB{Float64}(1, (i - 5*r)/r, 0)
+        else
+            return RGB{Float64}(1, 0, 0)
+        end
+    end
+
+    iteration = 200
+
+
     for i = 1:n
         for j = 1:m
             y = (2*i - n)/n * 1im
@@ -72,6 +65,6 @@ iteration = 100
         end
     end
 
-    # save("/Users/ambros.anrig/Documents/GitHub/Maturaarbeit_Ambros/Mandelbrotmenge.png", img)
+# save("/Users/ambros.anrig/Documents/GitHub/Maturaarbeit_Ambros/Mandelbrotmenge.png", img)
     save("/Users/Ambros D. Anrig/OneDrive - Kanton Glarus/Dokumente/GitHub/Maturaarbeit_Ambros/Mandelbrotmenge.png", img)
 end
